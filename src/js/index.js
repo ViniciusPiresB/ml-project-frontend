fa12a656822b0f556e5b85ab0bc93e1cc7e14ddf
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2023-02-12 19:10:02 -0300

    ✨ feat: Table from API

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,79 @@
-console.log("Teste");
+const APIUrl = "http://localhost:3000/";
+const ordersTable = document.getElementById("orders-table");
+
+const sleep = (s) => new Promise((res) => setTimeout(res, s * 1000));
+
+(async () => {
+  while (true) {
+    const ordersQuantityInHtml = document.getElementsByClassName("order");
+
+    const response = await fetch(APIUrl + "order");
+    const responseData = await response.json();
+
+    const quantityOfOrdersFromApi = responseData.length;
+    const quantityOfOrdersInHtml = ordersQuantityInHtml.length;
+
+    if (quantityOfOrdersFromApi != quantityOfOrdersInHtml) {
+      console.log("Updating table...");
+      generateTable(responseData);
+    }
+
+    await sleep(4);
+  }
+})();
+
+const generateTable = async (orders) => {
+  const keys = ["title", "quantity", "username", "date_of_order", "variation"];
+
+  const tbl = document.getElementById("table");
+  const tblExistingBody = document.getElementById("tbody");
+
+  const tblBody = document.createElement("tbody");
+  tblBody.setAttribute("id", "tbody");
+
+  for (let i = 0; i < orders.length; i++) {
+    const row = document.createElement("tr");
+    row.classList.add("order");
+
+    for (let j = 0; j < 5; j++) {
+      if (keys[j] == "variation") {
+        const variations = orders[i][keys[j]];
+
+        const nameCell = document.createElement("td");
+        for (let k = 0; k < variations.length; k++) {
+          const nameTextCell = document.createTextNode(variations[k].name);
+
+          nameCell.appendChild(nameTextCell);
+        }
+        row.appendChild(nameCell);
+
+        const valueCell = document.createElement("td");
+        for (let k = 0; k < variations.length; k++) {
+          const valueTextCell = document.createTextNode(variations[k].value);
+
+          valueCell.appendChild(valueTextCell);
+        }
+        row.appendChild(valueCell);
+
+        continue;
+      }
+
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(orders[i][keys[j]]);
+
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    tblBody.appendChild(row);
+  }
+
+  if (tblExistingBody) {
+    tbl.removeChild(tblExistingBody);
+  }
+
+  tbl.appendChild(tblBody);
+  ordersTable.appendChild(tbl);
+
+  tbl.setAttribute("border", "2px");
+};
